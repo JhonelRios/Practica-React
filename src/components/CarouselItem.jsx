895f768e080ff44fe5ab3eafad9643775f267a70
Jhonel Rios Jaimes <riosjaimes.j@gmail.com>
2020-03-02 19:17:31 -0500

    Uso de webpack para empaquetar archivos estaticos, uso de hooks y propTypes

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,18 +1,29 @@
 import React from "react";
+import PropTypes from "prop-types";
 import "../assets/styles/components/CarouselItem.scss";
+import playIcon from "../assets/static/play-icon.png";
+import plusIcon from "../assets/static/plus-icon.png";
 
-const CarouselItem = () => (
+const CarouselItem = ({ cover, title, year, contentRating, duration }) => (
     <div className="carousel-item">
-        <img className="carousel-item__img" src="https://images.pexels.com/photos/1438072/pexels-photo-1438072.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="gente"/>
+        <img className="carousel-item__img" src={cover} alt={title}/>
         <div className="carousel-item__details">
             <div>
-                <img src="images/play-icon.png" alt="play"/>
-                <img src="images/plus-icon.png" alt="plus"/>
+                <img src={playIcon} alt="play"/>
+                <img src={plusIcon} alt="plus"/>
             </div>
-            <p className="carousel-item__details--title">Titulo descriptivo</p>
-            <p className="carousel-item__details--subtitle">2019 16+ 114 minutos</p>
+            <p className="carousel-item__details--title">{title}</p>
+            <p className="carousel-item__details--subtitle">{`${year} ${contentRating} ${duration}min`}</p>
         </div>
     </div>
 );
+
+CarouselItem.propTypes = {
+    cover: PropTypes.string,
+    title: PropTypes.string,
+    year: PropTypes.number,
+    contentRating: PropTypes.string,
+    duration: PropTypes.number,
+}
 
 export default CarouselItem;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-import "../assets/styles/components/Header.scss"
+import "../assets/styles/components/Header.scss";
+import logo from "../assets/static/logo-platzi-video.png";
+import userIcon from "../assets/static/usuario-64.png";
 
 const Header = () => (
     <header className="header">
-        <img className="header__img" alt="PlatziVideo"/>
+        <img className="header__img" src={logo} alt="PlatziVideo"/>
         <div className="header__menu">
             <div className="header__menu--profile">
-                <img alt="Profile Image"/>
+                <img src={userIcon} alt="Profile Image"/>
                 <p>Perfil</p>
             </div>
             <ul>
